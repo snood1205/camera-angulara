9af4fdec66a062d4f29f37e64ff3981327ddf2ce
Eli Sadoff <esadoff@ardiangroup.com>
2017-08-01 10:40:23 -0400

    Filling out ng-module to allow for usage in demo

--- src/camera-angulara.module.ts
+++ src/camera-angulara.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { CameraAngularaComponent } from './camera-angulara/camera-angulara.component'
+import { MediaModule } from './utilities/media-module/media.module'
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    MediaModule
+  ],
   providers: [
     {provide: 'Window', useValue: window},
     {provide: 'Navigator', useValue: navigator}
-  ]
+  ],
+  declarations: [CameraAngularaComponent],
+  exports: [CameraAngularaComponent],
+  entryComponents: [CameraAngularaComponent]
 })
 export class CameraAngularaModule {
 }
