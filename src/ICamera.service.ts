8f8d877ff653fee300d5768f71b72cd35f4754ec
Eli Sadoff <esadoff@ardiangroup.com>
2017-08-01 15:26:44 -0400

    Add documentation

--- src/ICamera.service.ts
+++ src/ICamera.service.ts
@@ -1,5 +1,14 @@
 import { Observable } from 'rxjs/Observable'
 
+/**
+ * This is the minimum requirements for a service that uploads the photo
+ * that was taken.
+ */
 export interface ICameraService {
-  upload (image): Observable<object>
+  /**
+   * This method must upload the provided image to the server.
+   *
+   * @return {Observable<object>} an observable containing the server response including the image
+   */
+  upload(image): Observable<object>
 }
--- src/camera-angulara/camera-angulara.component.ts
+++ src/camera-angulara/camera-angulara.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, Input, OnInit } from '@angular/core'
+import { Component, Inject, EventEmitter, Input, Output, OnInit } from '@angular/core'
 import { IDimensions } from '../utilities/interfaces/IDimensions'
 import { ICameraService } from '../ICamera.service'
 
@@ -14,37 +14,64 @@ import { ICameraService } from '../ICamera.service'
           <img alt="User's screen capture">
       </div>`
 })
+/**
+ * The main component for camera angulara. It has 5 possible inputs and is what
+ * the end user should use in their application as 'camera-angulara-component'.
+ *
+ * @author Eli Sadoff <snood1205@gmail.com>
+ */
 export class CameraAngularaComponent implements OnInit {
+  /**
+   * The number of seconds you want for the camera to countdown before the picture takes.
+   */
   @Input() countdown: number
-  @Input() width: number = 320
-  @Input() height: number = 0
-  @Input() imageFormat: string
-  @Input() captureMessage: string
-  // @Output() onPhotoCapture: EventEmitter
 
+  /**
+   * The width of the image in pixels.
+   */
+  @Input() width: number = 320
+
+  /**
+   * The height of the image in pixels.
+   */
+  @Input() height: number = 0
+
+  /**
+   * The desired format of the image (as in JPEG, PNG, etc).
+   */
+  @Input() imageFormat: string
+
+  /**
+   * The message to be displayed upon capture.
+   */
+  @Input() captureMessage: string
+  @Output() onPhotoCapture: EventEmitter<object> = new EventEmitter<object>()
+
+  image: ImageBitmap
   stream: MediaStream
   streaming: boolean = false
   canvas: IDimensions
   video: IDimensions
   videoObj: object
 
-  constructor (@Inject('Navigator') private navigator: Navigator, private cameraService: ICameraService) {
+  constructor( @Inject('Navigator') private navigator: Navigator, private cameraService: ICameraService) {
   }
 
-  ngOnInit () {
-    this.video = {height: this.height, width: this.width}
-    this.canvas = {height: this.height, width: this.width}
-    this.navigator.mediaDevices.getUserMedia(({video: true, audio: false}))
+  ngOnInit() {
+    this.video = { height: this.height, width: this.width }
+    this.canvas = { height: this.height, width: this.width }
+    this.navigator.mediaDevices.getUserMedia(({ video: true, audio: false }))
       .then((stream) => this.stream = stream)
   }
 
-  streamOnCanPlay () {
+  streamOnCanPlay() {
     if (!this.streaming) {
       this.streaming = true
     }
   }
 
-  takePhoto () {
+  takePhoto() {
     this.cameraService.upload(this.videoObj)
+    this.onPhotoCapture.emit(this.image)
   }
 }
