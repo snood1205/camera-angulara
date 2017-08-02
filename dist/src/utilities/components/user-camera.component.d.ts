6620b87c21687b6ee7814d1030773f1c390a3ef9
Eli Sadoff <esadoff@ardiangroup.com>
2017-08-02 15:13:44 -0400

    (fix): user-camera dist not added

---
+++ dist/src/utilities/components/user-camera.component.d.ts
@@ -0,0 +1,5 @@
+import { ParentCameraComponent } from './parent-camera.component';
+import { ICameraService } from '../services/ICamera.service';
+export declare class UserCameraComponent extends ParentCameraComponent {
+    protected cameraService: ICameraService;
+}
---
+++ dist/src/utilities/components/user-camera.component.js
@@ -0,0 +1,35 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+import { Component } from '@angular/core';
+import { ParentCameraComponent } from './parent-camera.component';
+/**
+ * This class should be extended by the user to implement the class they use.
+ * It should have a template similar to this:
+ * {@code `
+ * <camera-angulara-component [height]="height" [width]="width" [countdown]="countdown"
+ *                            [imageFormat]="imageFormat" captureMessage="captureMessage">
+ * </camera-angulara-component>
+ * `}
+ */
+var UserCameraComponent = (function (_super) {
+    __extends(UserCameraComponent, _super);
+    function UserCameraComponent() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    UserCameraComponent.decorators = [
+        { type: Component, args: [{ template: '' },] },
+    ];
+    /** @nocollapse */
+    UserCameraComponent.ctorParameters = function () { return []; };
+    return UserCameraComponent;
+}(ParentCameraComponent));
+export { UserCameraComponent };
+//# sourceMappingURL=user-camera.component.js.map
