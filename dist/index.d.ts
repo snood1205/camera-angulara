7686e1aadbf1e56b0b21d798027a6d50b6cccd29
Eli Sadoff <esadoff@ardiangroup.com>
2017-08-02 15:29:28 -0400

    (fix): export camera-angulara-component

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,4 @@
 export { CameraAngularaModule } from './src/camera-angulara.module';
 export { ICameraService } from './src/utilities/services/ICamera.service';
 export { UserCameraComponent } from './src/utilities/components/user-camera.component';
+export { CameraAngularaComponent } from './src/camera-angulara/camera-angulara.component';
--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,4 @@
 export { CameraAngularaModule } from './src/camera-angulara.module';
 export { UserCameraComponent } from './src/utilities/components/user-camera.component';
+export { CameraAngularaComponent } from './src/camera-angulara/camera-angulara.component';
 //# sourceMappingURL=index.js.map
