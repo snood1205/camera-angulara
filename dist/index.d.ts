b5e517398c41b1ad1116dc56a16545c488029b84
Eli Sadoff <esadoff@ardiangroup.com>
2017-08-02 14:55:27 -0400

    (fix): export UserCameraComponent in index

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,3 @@
 export { CameraAngularaModule } from './src/camera-angulara.module';
 export { ICameraService } from './src/utilities/services/ICamera.service';
+export { UserCameraComponent } from './src/user-camera.component';
--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,3 @@
 export { CameraAngularaModule } from './src/camera-angulara.module';
+export { UserCameraComponent } from './src/user-camera.component';
 //# sourceMappingURL=index.js.map
